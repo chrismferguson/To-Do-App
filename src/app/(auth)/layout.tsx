8e8becbba824a16b8d2ddc460e335b3390e32a76
Chris M Ferguson <chrismferguson@gmail.com>
2023-11-13 09:56:44 +0900

    Created new files, deleted other files, further developing ToDo app

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en" className={inter.className}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
